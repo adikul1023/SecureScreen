ad467f055175e7e8ac5f49404985f5c77280ea78
Adi K <102898613+adikul1023@users.noreply.github.com>
2026-05-04 11:42:39 +0530

    Disable dependency metadata in release APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,11 @@ android {
         }
     }
 
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
